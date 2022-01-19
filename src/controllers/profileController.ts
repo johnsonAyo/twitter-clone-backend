5c834b26e01c3248b1e915b141499f5868c0be82
adebayoakingbade <adebayo.akingbade@decagon.dev>
2022-01-19 15:50:47 +0100

    add user id on create profile

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -8,12 +8,11 @@ import imageMulter from '../utils/tweet_utils/multerImageUpload';
 import cloudinaryImage from '../utils/tweet_utils/cloudinaryImageStorage';
 import userModels from '../models/userModels';
 
-
 const upload = imageMulter.single('profilePicture');
 
 export const uploadProfilePicture = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    upload(req, res, async (err)  => {
+    upload(req, res, async (err) => {
       if (err instanceof multer.MulterError) {
         return next(new ErrorHandler(500, err.message));
       } else if (err) {
@@ -23,13 +22,13 @@ export const uploadProfilePicture = catchAsync(
       const path = req.file?.path;
       try {
         const file = await cloudinaryImage.uploader.upload(path as string);
-        const profile = await Profile.findOne({ _id: req.params.id })
+        const profile = await Profile.findOne({ _id: req.params.id });
         if (!profile) return next(new ErrorHandler(500, err.message));
         await profile.update({ profilePicture: file.url });
         return res.status(201).json({
-            status: 'successful!',
-            profile,
-        })
+          status: 'successful!',
+          profile,
+        });
       } catch (error: any) {
         next(new ErrorHandler(500, error.message));
       }
@@ -38,9 +37,10 @@ export const uploadProfilePicture = catchAsync(
 );
 
 export const createProfile = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const user = await userModels.findById(req.params.id);
-    if (!user) 
+  const user = await userModels.findById(req.params.id);
+  if (!user) return next(new ErrorHandler(404, 'User not found'));
   const newProfile = await Profile.create({
+    user: user.id,
     name: req.body.name,
     bio: req.body.bio,
     location: req.body.location,
@@ -55,16 +55,16 @@ export const createProfile = catchAsync(async (req: Request, res: Response, next
 });
 
 export const updateProfile = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const profile = await Profile.findById(req.params.id);
-    await profile?.update({
-        name: req.body.name || profile.name,
-        bio: req.body.bio || profile.bio,
-        location: req.body.location || profile.location,
-        birthday: req.body.birthday || profile.birthday,
-        website: req.body.website || profile.website,
-    })
-    return res.status(201).json({
-        status: 'successful!',
-        profile,
-    })
-})
+  const profile = await Profile.findById(req.params.id);
+  await profile?.update({
+    name: req.body.name || profile.name,
+    bio: req.body.bio || profile.bio,
+    location: req.body.location || profile.location,
+    birthday: req.body.birthday || profile.birthday,
+    website: req.body.website || profile.website,
+  });
+  return res.status(201).json({
+    status: 'successful!',
+    profile,
+  });
+});
