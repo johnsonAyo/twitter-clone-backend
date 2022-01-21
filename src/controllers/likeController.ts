0ba762c2b9b0706c24cb649922d8e994be45e161
iamLolade <adeniyiotemade@gmail.com>
2022-01-21 19:49:05 +0100

    Created like controller

--- src/controllers/likeController.ts
+++ src/controllers/likeController.ts
@@ -1,15 +1,19 @@
-import express, { Request, Response, NextFunction }from "express";
+import express, { Request, Response, NextFunction, urlencoded }from "express";
 import Like from "../models/likeModel";
-import Post from "../model/postModel"
+//import Post from "../model/postModel"
+
+const app = express();
+
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
 
 export const like_post = async (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id;
-    const userId = req.user._id
+    const { userId } = req.body;
     
     try {
-        const post = await Post.findById(id);
-        const like = await Like.create({post._id, userId})
-        // const result = await like.save()
+        //const post = await Post.findById(id);
+        const like = await Like.create({id, userId})
         res.status(200).json({message:"The post has been liked", data: like})
     } catch (error) {
         res.status(500).json(error);
@@ -18,9 +22,12 @@ export const like_post = async (req: Request, res: Response, next: NextFunction)
 
 export const unlike_post = async (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id;
+    console.log(req.body)
+    const { userId } = req.body;
 
     try {
-        const result = await Like.findByIdAndDelete(id)
+        const result = await Like.deleteOne({ postId: id, userId })
+        console.log(result)
         res.status(200).json("The post has been disliked")
     } catch (error) {
         res.status(500).json(error);
