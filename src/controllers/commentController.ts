807487557c77d189cf9e1f9c2cab751cf29714af
iamLolade <adeniyiotemade@gmail.com>
2022-01-25 17:37:54 +0100

    Restructured commentController to adapt catchAsync structure

--- src/controllers/commentController.ts
+++ src/controllers/commentController.ts
@@ -1,24 +1,24 @@
 import express, { Request, Response, NextFunction } from 'express';
 import Comment from '../models/commentModel';
+import catchAsync from '../utils/catchAsync';
+import ErrorHandler from '../utils/appError';
 
-export const commentTweet = async (req: Request, res: Response, _next: NextFunction) => {
-  const { id } = req.params;
-  const userId = req.user._id;
-  const { content } = req.body;
-
-  try {
+export const commentTweet = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const userId = req.user._id;
+    const { content } = req.body;
+  
     const comment = await Comment.create({ tweetId: id, userId, content });
+    if (!comment) return next(new ErrorHandler(400, 'Error occurred'));
     res.status(200).json({ message: 'Comment successful', data: comment });
-  } catch (error: any) {
-    res.status(400).json({ message: error.message });
   }
-};
+);
 
-export const getComments = async (req: Request, res: Response, _next: NextFunction) => {
-  try {
+export const getComments = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const comments = await Comment.find();
+    if (!comments) return next(new ErrorHandler(400, 'Error occurred'));
     res.status(200).json({ message: 'All comments', data: comments });
-  } catch (error: any) {
-    res.status(400).json({ message: error.message });
   }
-};
+);
