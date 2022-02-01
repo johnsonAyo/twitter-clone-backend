15aa387545cae31f5dfb7130e33ed7a2d6a41712
chidera okara <chidera.okara@decagon.dev>
2022-02-01 17:15:57 +0100

    feat/view-friend-tweet

--- src/models/viewtweetModel.ts
+++ src/models/viewtweetModel.ts
@@ -22,12 +22,14 @@ export async function viewTweet(userId: string, pageNo: number, pageSize: number
   let output: any = { following: followingId, tweet: data, retweet };
   return output;
 }
-export async function viewTweetofFriend(userId: string, pageNo: number, pageSize: number): Promise<void> {
-  let following: any = await Follow.find({ followId: userId })
+export async function viewTweetofFriend(
+  userId: string,
+  pageNo: number,
+  pageSize: number,
+): Promise<void> {
+  let data: any = await CreateTweetCln.find({ userId: userId })
     .skip(pageNo - 1)
     .limit(pageSize);
-  let followingId = following.map((val: any) => val.userId);
 
-  let data: any = await CreateTweetCln.find({ userId: { $in: followingId } });
-  return dataCheck;
+  return data;
 }
