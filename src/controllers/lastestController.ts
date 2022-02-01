781ca7e962bbb9d773093e0a0b948fd16c1e97f0
iamLolade <adeniyiotemade@gmail.com>
2022-02-01 17:55:14 +0100

    Created controller for latest tweet feature

---
+++ src/controllers/lastestController.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response, NextFunction } from 'express';
+import CreateTweetCln from '../models/tweetModel';
+import catchAsync from '../utils/catchAsync';
+import ErrorHandler from '../utils/appError';
+
+export const getLatest = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+    const tweets = await CreateTweetCln
+                                .find()
+                                .sort({
+                                    updatedAt: -1
+                                }).populate("tweetId")
+    if(!tweets) return next(new ErrorHandler(400, "Error occurred"));
+    res.status(200).json({message: "Latest tweets", data: tweets})
+
+})
+
