c4d9e5af57ff5274c5402481379bca5bc1c44c73
naheem adedokun <naheemadedokun21@gmail.com>
2022-02-04 20:00:12 +0100

    updates

---
+++ src/__tests__/search.test.ts
@@ -0,0 +1,58 @@
+import supertest from 'supertest';
+import app from '../app';
+
+let emailToken: string;
+let token: string;
+
+describe('Following Feature Test Case', () => {
+  const userData = {
+    firstName: 'Tolu',
+    lastName: 'Johnson',
+    email: 'tolz@yahoo.com',
+    password: 'testing',
+  };
+  
+  test('signup', async () => {
+    const response = await supertest(app).post('/users/signup').send(userData);
+
+    emailToken = response.body.emailToken;
+
+    expect(response.status).toBe(200);
+    expect(response.body.newUser.isActive).toBe(false);
+  });
+
+  test('confirmEmail', async () => {
+    const response = await supertest(app).get(`/users/verify/${emailToken}`);
+
+    expect(response.status).toBe(201);
+    expect(response.body.emailToken.email).toEqual(response.body.data.email);
+    expect(response.body.data.isActive).toBe(true);
+  });
+
+  test('login', async () => {
+    const response = await supertest(app)
+      .post('/users/login')
+      .send({ email: userData.email, password: userData.password });
+
+    token = response.body.token;
+
+    expect(response.status).toBe(201);
+    expect(response.body.user.isActive).toBe(true);
+  });
+
+  it('Searches for tweets and comments', async() => {
+      const response = await supertest(app)
+      .get(`/api/v1/search`)
+      .set('Authorization', `Bearer ${token}`)
+      expect(response.status).toBe(200)
+      expect(response.body.message).toBe('success')
+  })
+  it('Searches for users', async() => {
+    const response = await supertest(app)
+    .get(`/api/v1/search/users`)
+    .set('Authorization', `Bearer ${token}`)
+    expect(response.status).toBe(200)
+    expect(response.body.message).toBe('success')
+})
+  
+});
