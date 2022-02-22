6b8d3cd0fe7e62d5f1defdcecd019354b094f5bd
ambiti0n <fabulousfresh010@gmail.com>
2022-02-22 22:19:56 +0100

    recent-update

--- src/socketapi.ts
+++ src/socketapi.ts
@@ -46,44 +46,6 @@ const Socketapi = () => {
       app.io.emit('getUsers', users);
     });
   });
-
-  // // socket auth middleware
-  // app.io.use(async (socket, next) => {
-  //   const token = socket.handshake.auth.token;
-
-  //   try {
-  //     const decodedToken: any = jwt.verify(
-  //       token as string,
-  //       process.env.JWT_SECRET as string
-  //     );
-
-  //     if (decodedToken) {
-  //       const user = await User.findById(decodedToken.id);
-
-  //       if (!user) {
-  //         return next(new Error("user no longer exist"));
-  //       }
-
-  //       // add to online logged in users
-  //       addToLoggedIn(socket.id, user._id);
-
-  //       // get users following the newly logged in user from DB
-  //       // const userFollowers = await Follower.findOne({ userId: user._id });
-
-  //       // get users following the newly logged in user from online logged in user
-  //       // const friends = getFriends(userFollowers.followers);
-
-  //       // notify the followers of the logged in user that a friend just logged in
-  //       // friends.forEach((friend) => {
-  //       //   app.io.to(friend.socketId).emit("new-login", user);
-  //       // });
-
-  //       next();
-  //     } else {
-  //       return next(new Error("invalid login credentials"));
-  //     }
-  //   } catch (e) {
-  //     return next(new Error("invalid login credentials"));
 };
 
 export default Socketapi;
